79d0144e3f914a3db94cdd051063d83dcf9180d9
KrLite <68179735+KrLite@users.noreply.github.com>
2023-12-30 17:20:30 +0800

    partly done

--- src/main/java/net/krlite/verticality/mixin/GameMenuScreenMixin.java
+++ src/main/java/net/krlite/verticality/mixin/GameMenuScreenMixin.java
@@ -22,6 +22,19 @@ public abstract class GameMenuScreenMixin extends Screen {
         super(title);
     }
 
+    @Unique
+    private static final Text
+            leftArrow = Text.of("<"),
+            rightArrow = Text.of(">"),
+            upDownArrow = Text.of("⬍"),
+            alternativeLayoutArrowWhenEnabled = Text.of("↓"),
+            alternativeLayoutArrowWhenDisabled = Text.of("←");
+
+    @Unique
+    private static Text alternativeLayoutArrow() {
+        return Verticality.enabled() ? alternativeLayoutArrowWhenEnabled : alternativeLayoutArrowWhenDisabled;
+    }
+
     @Inject(method = "render", at = @At("RETURN"))
     private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         double progress = Math.pow(Verticality.progress(), 2), alternativeProgress = Math.pow(Verticality.alternativeProgress(), 2);
@@ -31,31 +44,35 @@ public abstract class GameMenuScreenMixin extends Screen {
                 (int) Math.max(5, (mouseInWidgetEnabled(mouseX, mouseY) ? alphaHovered : alpha) * (1 - progress)));
         Color colorUpsideDown = new Color(255, 255, 255,
                 (int) Math.max(5, (mouseInWidgetUpsideDown(mouseX, mouseY) ? alphaHovered : alpha) * (1 - progress)));
-        Color colorAlternative = new Color(255, 255, 255,
-                (int) Math.max(5, (mouseInWidgetAlternativeMode(mouseX, mouseY) ? alphaHovered : alpha) * (1 - progress)));
+        Color colorAlternativeLayout = new Color(255, 255, 255,
+                (int) Math.max(5, (mouseInWidgetAlternativeLayout(mouseX, mouseY) ? alphaHovered : alpha) * (1 - progress)));
+
+        RenderSystem.enableDepthTest();
+        RenderSystem.disableCull();
+
+        context.getMatrices().push();
+        context.getMatrices().translate(
+                Verticality.enabled() ? Verticality.raisedShift() : 0,
+                Verticality.enabled() ? 0 : -Verticality.raisedShift(),
+                0
+        );
 
         if (Verticality.enabled()) {
-            RenderSystem.enableDepthTest();
-            RenderSystem.disableCull();
-
-            context.getMatrices().push();
-            context.getMatrices().translate(Verticality.raisedShift(), 0, 0);
-
             // 'Disable' widget
             widgetDisable:
             {
                 context.getMatrices().push();
                 context.getMatrices().translate(
                         13 * (1 - Verticality.transition()),
-                        Theory.lerp(height / 2.0F, height - (22 + 13 * (1 - Verticality.transition())), Verticality.alternativeProgress()),
+                        Theory.lerp(height / 2.0F, height - (31 + 13), Verticality.alternativeProgress()),
                         0
                 );
                 context.getMatrices().scale(Verticality.SCALAR, Verticality.SCALAR, Verticality.SCALAR);
                 context.getMatrices().translate(0, Verticality.FONT_GAP_OFFSET, 0);
 
                 context.drawText(
-                        textRenderer, right,
-                        (int) (-textRenderer.getWidth(right) / 2.0F * (float) (1 - Verticality.transition())),
+                        textRenderer, rightArrow,
+                        (int) (-textRenderer.getWidth(rightArrow) / 2.0F * (1 - Verticality.transition())),
                         (int) (-(textRenderer.fontHeight - 1) / 2.0F),
                         colorEnabled.getRGB(), false
                 );
@@ -69,55 +86,29 @@ public abstract class GameMenuScreenMixin extends Screen {
                 context.getMatrices().push();
                 context.getMatrices().translate(
                         13 + 17 * (1 - Verticality.transition()),
-                        Theory.lerp(height / 2.0F, height - (22 + 13 * (1 - Verticality.transition())), Verticality.alternativeProgress()),
+                        Theory.lerp(height / 2.0F, height - 13, Verticality.alternativeProgress()),
                         500
                 );
-                context.getMatrices().multiply(RotationAxis.POSITIVE_Z.rotationDegrees(90));
-                context.getMatrices().multiply(RotationAxis.POSITIVE_Y.rotationDegrees((float) (180 * Verticality.swap())));
+                context.getMatrices().scale(Verticality.SCALAR, Verticality.SCALAR, Verticality.SCALAR);
+                context.getMatrices().multiply(RotationAxis.POSITIVE_X.rotationDegrees((float) (180 * Verticality.swap())));
+
+                context.getMatrices().translate(0, Verticality.FONT_GAP_OFFSET, 0);
 
                 context.drawText(
-                        textRenderer, both,
-                        (int) (-textRenderer.getWidth(both) / 2.0F),
-                        (int) (-textRenderer.fontHeight / 2.0F * (float) (1 - Verticality.transition())),
+                        textRenderer, upDownArrow,
+                        (int) (-textRenderer.getWidth(upDownArrow) / 2.0F),
+                        (int) (-textRenderer.fontHeight / 2.0F * (1 - Verticality.transition())),
                         colorUpsideDown.getRGB(),
                         false
                 );
 
                 context.getMatrices().pop();
             }
-
-            // 'Alternative Mode'
-            widgetAlternative:
-            {
-                context.getMatrices().push();
-                context.getMatrices().translate(
-                        13 * (1 - Verticality.transition()),
-                        height - 13,
-                        500
-                );
-                context.getMatrices().scale(Verticality.SCALAR, Verticality.SCALAR, Verticality.SCALAR);
-                context.getMatrices().translate(0, Verticality.FONT_GAP_OFFSET, 0);
-                context.getMatrices().multiply(RotationAxis.POSITIVE_X.rotationDegrees((float) (180 * alternativeProgress)));
-
-                context.drawText(
-                        textRenderer, alternativeMode,
-                        (int) (-textRenderer.getWidth(alternativeMode) / 2.0F * (float) (1 - Verticality.transition())),
-                        (int) (-(textRenderer.fontHeight - 1) / 2.0F),
-                        colorAlternative.getRGB(), false
-                );
-
-                context.getMatrices().pop();
-            }
-
-            context.getMatrices().pop();
-
-            RenderSystem.enableCull();
         } else {
             // 'Enable'
             context.getMatrices().push();
-            context.getMatrices().translate(0, -Verticality.raisedShift(), 0);
             context.getMatrices().translate(
-                    width / 2.0F,
+                    Theory.lerp(width / 2.0F, 31 + 13, Verticality.alternativeProgress()),
                     height - 10 * (1 - Verticality.transition()),
                     0
             );
@@ -125,25 +116,53 @@ public abstract class GameMenuScreenMixin extends Screen {
             context.getMatrices().translate(-Verticality.FONT_GAP_OFFSET, 0, 0);
 
             context.drawText(
-                    textRenderer, left,
-                    (int) (-textRenderer.getWidth(left) / 2.0F),
-                    (int) (-textRenderer.fontHeight / 2.0F * (float) (1 - Verticality.transition())),
+                    textRenderer, leftArrow,
+                    (int) (-textRenderer.getWidth(leftArrow) / 2.0F),
+                    (int) (-textRenderer.fontHeight / 2.0F * (1 - Verticality.transition())),
                     colorEnabled.getRGB(), false
             );
 
             context.getMatrices().pop();
         }
+
+        // 'Alternative Layout'
+        widgetAlternativeLayout:
+        {
+            context.getMatrices().push();
+            context.getMatrices().translate(
+                    Verticality.enabled() ? 13 * (1 - Verticality.transition()) : 10,
+                    height - (Verticality.enabled() ? 13 : (10 * (1 - Verticality.transition()))),
+                    500
+            );
+            context.getMatrices().scale(Verticality.SCALAR, Verticality.SCALAR, Verticality.SCALAR);
+            context.getMatrices().multiply((Verticality.enabled() ? RotationAxis.POSITIVE_X : RotationAxis.POSITIVE_Y)
+                    .rotationDegrees((float) (180 * alternativeProgress)));
+
+            context.getMatrices().translate(
+                    Verticality.enabled() ? 0 : Verticality.FONT_GAP_OFFSET,
+                    Verticality.enabled() ? Verticality.FONT_GAP_OFFSET : 0,
+                    0
+            );
+
+            context.drawText(
+                    textRenderer, alternativeLayoutArrow(),
+                    (int) (-textRenderer.getWidth(alternativeLayoutArrow()) / 2.0F),
+                    (int) (-textRenderer.fontHeight / 2.0F),
+                    colorAlternativeLayout.getRGB(), false
+            );
+
+            context.getMatrices().pop();
+        }
+
+        context.getMatrices().pop();
+
+        RenderSystem.enableCull();
     }
 
     @Unique
     private boolean mouseIn(double mouseX, double mouseY, double xCentered, double yCentered, double w, double h) {
         return mouseX >= xCentered - w / 2 && mouseX <= xCentered + w / 2 && mouseY >= yCentered - h / 2 && mouseY <= yCentered + h / 2;
-    }    @Unique
-    private static final Text
-            left = Text.of("<"),
-            right = Text.of(">"),
-            both = left.copy().append(" ").append(right),
-            alternativeMode = Text.of("↓");
+    }
 
     @Unique
     private boolean mouseInWidgetEnabled(double mouseX, double mouseY) {
@@ -154,32 +173,32 @@ public abstract class GameMenuScreenMixin extends Screen {
             return mouseIn(
                     mouseX, mouseY,
                     13 + Verticality.raisedShift(),
-                    Verticality.alternativeLayoutEnabled() ? height - (22 + 13 + Verticality.raisedShift()) : height / 2.0F,
-                    textRenderer.getWidth(right) * Verticality.SCALAR,
+                    Verticality.alternativeLayoutEnabled() ? height - (31 + 13) : height / 2.0F + Verticality.FONT_GAP_OFFSET,
+                    textRenderer.getWidth(rightArrow) * Verticality.SCALAR,
                     textRenderer.fontHeight * Verticality.SCALAR
             );
         } else {
             // 'Enable'
             return mouseIn(
                     mouseX, mouseY,
-                    width / 2.0F,
+                    Verticality.alternativeLayoutEnabled() ? 31 + 13 : width / 2.0F - Verticality.FONT_GAP_OFFSET,
                     height - (10 + Verticality.raisedShift()),
-                    textRenderer.getWidth(left) * Verticality.SCALAR,
+                    textRenderer.getWidth(leftArrow) * Verticality.SCALAR,
                     textRenderer.fontHeight * Verticality.SCALAR
             );
         }
     }
 
     @Unique
-    private boolean mouseInWidgetAlternativeMode(double mouseX, double mouseY) {
-        if (Verticality.unavailable() || !Verticality.enabled()) return false;
+    private boolean mouseInWidgetAlternativeLayout(double mouseX, double mouseY) {
+        if (Verticality.unavailable()) return false;
 
         return mouseIn(
                 mouseX, mouseY,
-                13 + Verticality.raisedShift(),
-                height - (13 + Verticality.raisedShift()),
-                textRenderer.fontHeight,
-                textRenderer.getWidth(alternativeMode)
+                Verticality.enabled() ? 13 + Verticality.raisedShift() : (10 - Verticality.FONT_GAP_OFFSET),
+                height - (Verticality.enabled() ? 13 - Verticality.FONT_GAP_OFFSET : (10 + Verticality.raisedShift())),
+                textRenderer.getWidth(alternativeLayoutArrow()) * Verticality.SCALAR,
+                textRenderer.fontHeight * Verticality.SCALAR
         );
     }
 
@@ -190,10 +209,10 @@ public abstract class GameMenuScreenMixin extends Screen {
 
         return mouseIn(
                 mouseX, mouseY,
-                30 + Verticality.raisedShift(),
-                Verticality.alternativeLayoutEnabled() ? height - (22 + 13 + Verticality.raisedShift()) : height / 2.0F,
-                textRenderer.fontHeight,
-                textRenderer.getWidth(both)
+                13 + 17 + Verticality.raisedShift(),
+                Verticality.alternativeLayoutEnabled() ? height - (13 - Verticality.FONT_GAP_OFFSET) : height / 2.0F,
+                textRenderer.getWidth(upDownArrow) * Verticality.SCALAR,
+                textRenderer.fontHeight * Verticality.SCALAR
         );
     }
 
@@ -204,7 +223,7 @@ public abstract class GameMenuScreenMixin extends Screen {
             Verticality.switchEnabled();
         }
 
-        if (mouseInWidgetAlternativeMode(mouseX, mouseY)) {
+        if (mouseInWidgetAlternativeLayout(mouseX, mouseY)) {
             Verticality.Sounds.playGateLatch();
             Verticality.switchAlternativeMode();
         }
@@ -216,8 +235,4 @@ public abstract class GameMenuScreenMixin extends Screen {
 
         return super.mouseClicked(mouseX, mouseY, button);
     }
-
-
-
-
 }
